85de4f0ae1701ce2bdd71931a5bb88606c26e1e2
SAURABH <sgupta8mz@gmail.com>
2023-09-07 20:16:13 +0530

    Day 7 commit

--- Disney-Hotstar/script.js
+++ Disney-Hotstar/script.js
@@ -3,14 +3,29 @@
 window.onload=function()
 {
 
-        
-    let ml=1715;
+    
     let fullWidth=document.getElementById('slider').offsetWidth;
-    let remainingWidth=fullWidth;
+    let totalCount=(fullWidth/1715)-1;
+
+    let maxSlides=Math.floor(totalCount);
+    let lastSlide=totalCount-maxSlides;
+
+    let currentSlide=0;
 
     document.getElementById("swipe-left").addEventListener("click",function(){
 
-    
+        if(currentSlide>=1 && currentSlide!=totalCount)
+        {
+            currentSlide--;
+             
+        }
+        else if(currentSlide==totalCount && lastSlide!==0)
+        {
+            currentSlide-=lastSlide;
+        }
+
+        document.getElementById("slider").style.marginLeft=-(currentSlide*1715)+"px";
+        console.log(currentSlide,totalCount,lastSlide);
 
     })
 
@@ -19,23 +34,20 @@ window.onload=function()
 
     document.getElementById("swipe-right").addEventListener("click",function(){
 
+        if(currentSlide<maxSlides)
+        {
+            currentSlide++;
+             
+        }
+        else if(currentSlide==maxSlides && lastSlide!==0)
+        {
+            currentSlide+=lastSlide;
+        }
+
+        document.getElementById("slider").style.marginLeft=-(currentSlide*1715)+"px";
        
-
-        if(remainingWidth>ml)
-        {
-            document.getElementById('slider').style.marginLeft=-ml+"px";
-            remainingWidth=remainingWidth-1715;
-            ml=ml+1715;
-            
-        }
-        else if(remainingWidth<ml)
-        {
-            document.getElementById('slider').style.marginLeft=-(fullWidth-remainingWidth)+"px";
-        }
-
-        console.log(ml,remainingWidth);
-        
-        
+       
+        console.log(currentSlide,totalCount,lastSlide);
 
     })
 
